075acf4b912851604de8a9a614018f3b7748d7ef
Kornilios Kourtis <kkourt@cslab.ece.ntua.gr>
2011-01-20 01:24:09 +0200

    added mempools

--- Makefile
+++ Makefile
@@ -2,11 +2,14 @@
 .PHONY: clean
 
 CC=gcc
-CFLAGS= -Wall -O2
+CFLAGS= -Wall -O2 -DNDEBUG
 
-all: rle_rec prle_rec rle_rec-SKEL
+all: rle_rec prle_rec rle_rec-mempools prle_rec-mempools
 
 rle_rec: rle_rec.cilk
+	$(CC) -xc -DNO_CILK $(CFLAGS) $< -o $@
+
+rle_rec-mempools: rle_rec-mempools.cilk
 	$(CC) -xc -DNO_CILK $(CFLAGS) $< -o $@
 
 rle_rec-SKEL: rle_rec-SKEL.cilk
@@ -15,5 +18,8 @@ rle_rec-SKEL: rle_rec-SKEL.cilk
 prle_rec: rle_rec.cilk
 	cilkc -cilk-critical-path $(CFLAGS) $< -o $@
 
+prle_rec-mempools: rle_rec-mempools.cilk
+	cilkc -cilk-critical-path $(CFLAGS) $< -o $@
+
 clean:
-	rm -f rle_rec prle_rec rle_rec-SKEL
+	rm -f rle_rec prle_rec prle_rec-mempools rle_rec-mempools
--- prle_rec_bench.sh
+++ prle_rec_bench.sh
@@ -1,47 +1,62 @@
 #!/bin/bash
 
-#export RLE_REC_LIMIT=1
-#for i in $(seq 1 8)
-#do
-#echo ./prle_rec -nproc $i
-#     ./prle_rec -nproc $i
-#echo
-#done
-
 rles=1000000
+threads=8
+
 rm -f Temp-C Temp-cilk Temp-results
 
-# get simple C measurements
-echo "*** Running serial (pure C) ***"
-./rle_rec $rles | tee Temp-C | sed -e 's/^/  > /'
+do_bench()
+{
+	if [ ! -e $CRLE ]; then
+		echo "**** $CRLE does not exist ****"
+		return
+	fi
+	if [ ! -e $PRLE ]; then
+		echo "**** $PRLE does not exist ****"
+		return
+	fi
 
-c_rle_enc=$(perl -n -e 'if (/^rle_encode:\s+(\S+) secs/){ print $1; }' < Temp-C)
-c_rle_rec_enc=$(perl -n -e 'if (/^rle_encode_rec:\s+(\S+) secs/){ print $1; }' < Temp-C)
+	echo
+	echo "*** Running serial (pure C) ($CRLE) ***" | tee -a Temp-results
+	echo $CRLE $rles
+	     $CRLE $rles | tee Temp-C | sed -e 's/^/  > /'
 
-echo "C RLE encode:           $c_rle_enc"      | tee -a Temp-results
-echo "C RLE recursive encode: $c_rle_rec_enc"  | tee -a Temp-results
+	c_rle_enc=$(perl -n -e 'if (/^rle_encode:\s+(\S+) secs/){ print $1; }' < Temp-C)
+	c_rle_rec_enc=$(perl -n -e 'if (/^rle_encode_rec:\s+(\S+) secs/){ print $1; }' < Temp-C)
+
+	echo "C RLE encode:           $c_rle_enc"      | tee -a Temp-results
+	echo "C RLE recursive encode: $c_rle_rec_enc"  | tee -a Temp-results
+	echo
+
+	echo $PRLE $rles
+	     $PRLE $rles | tee Temp-cilk | sed -e 's/^/  > /'
+	cilk_rle_enc_serial=$(perl -n -e 'if (/^rle_encode:\s+(\S+) secs/){ print $1; }' < Temp-cilk)
+	cilk_rle_rec_enc_serial=$(perl -n -e 'if (/^rle_encode_rec:\s+(\S+) secs/){ print $1; }' < Temp-cilk)
+
+
+	echo "*** Running parallel (Cilk) ($PRLE) ***" | tee -a Temp-results
+	for i in $(seq 1 $threads)
+	do
+		echo $PRLE --nproc $i $rles
+		     $PRLE --nproc $i $rles | tee Temp-cilk | sed -e 's/^/  > /'
+		cilk_rle_rec_enc=$(perl -n -e 'if (/^rle_encode_rec:\s+(\S+) secs/){ print $1; }' < Temp-cilk)
+		p1=$(printf "%2.2lf\n" $(echo "$c_rle_enc               / $cilk_rle_rec_enc"  | bc -l))
+		p2=$(printf "%2.2lf\n" $(echo "$c_rle_rec_enc           / $cilk_rle_rec_enc"  | bc -l))
+		p3=$(printf "%2.2lf\n" $(echo "$cilk_rle_enc_serial     / $cilk_rle_rec_enc"  | bc -l))
+		p4=$(printf "%2.2lf\n" $(echo "$cilk_rle_rec_enc_serial / $cilk_rle_rec_enc"  | bc -l))
+		echo "Cilk RLE (thr:$i):       $cilk_rle_rec_enc (vs C: $p1) (vs C-rec: $p2) (vs cilk-serial:$p3) (vs cillk-serial-rec:$p4)"  | tee -a Temp-results
+	done
+	cilk_rle_limit=$(perl -n -e 'if (/^rle_rec_limit:\s*(\S+)/){ print $1; }' < Temp-cilk)
+}
+
+
+CRLE="./rle_rec"; PRLE="./prle_rec"
+do_bench
+
+CRLE="./rle_rec-mempools"; PRLE="./prle_rec-mempools"
+do_bench
+
 echo
-
-
-echo ./prle_rec $rles
-     ./prle_rec $rles | tee Temp-cilk | sed -e 's/^/  > /'
-cilk_rle_enc_serial=$(perl -n -e 'if (/^rle_encode:\s+(\S+) secs/){ print $1; }' < Temp-cilk)
-cilk_rle_rec_enc_serial=$(perl -n -e 'if (/^rle_encode_rec:\s+(\S+) secs/){ print $1; }' < Temp-cilk)
-
-echo "*** Running parallel (Cilk) ***"
-for i in $(seq 1 8)
-do
-	echo ./prle_rec --nproc $i $rles
-	     ./prle_rec --nproc $i $rles | tee Temp-cilk | sed -e 's/^/  > /'
-	cilk_rle_rec_enc=$(perl -n -e 'if (/^rle_encode_rec:\s+(\S+) secs/){ print $1; }' < Temp-cilk)
-	p1=$(printf "%2.2lf\n" $(echo "$c_rle_enc / $cilk_rle_rec_enc"  | bc -l))
-	p2=$(printf "%2.2lf\n" $(echo "$c_rle_rec_enc / $cilk_rle_rec_enc"  | bc -l))
-	p3=$(printf "%2.2lf\n" $(echo "$cilk_rle_enc_serial / $cilk_rle_rec_enc"  | bc -l))
-	p4=$(printf "%2.2lf\n" $(echo "$cilk_rle_rec_enc_serial / $cilk_rle_rec_enc"  | bc -l))
-	echo "Cilk RLE (thr:$i):       $cilk_rle_rec_enc (vs C: $p1) (vs C-rec: $p2) (vs cilk-serial:$p3) (vs cillk-serial-rec:$p4)"  | tee -a Temp-results
-done
-cilk_rle_limit=$(perl -n -e 'if (/^rle_rec_limit:\s*(\S+)/){ print $1; }' < Temp-cilk)
-
 echo "*** Results ***"
 echo "Number of RLES:         $rles"
 echo "RLE recursion limit:    $cilk_rle_limit"
