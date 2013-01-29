286a27e02a6e8e26d0ec41eae42054a7136c9429
Kornilios Kourtis <kkourt@inf.ethz.ch>
2013-01-29 15:02:20 +0100

    scripts/bench_rle: add a sla_max_level parameter

--- scripts/bench_rle.sh
+++ scripts/bench_rle.sh
@@ -9,6 +9,7 @@ source scripts/utils.sh
 rle_rec_limit_S="128 256 512"
 xarr_rle_grain_S="16 32 64"
 rles_S="500000 1000000 5000000"
+sla_max_level_S="5 15 20"
 
 repeats=3
 resdir="/dev/shm"
@@ -25,17 +26,36 @@ git_info     > $resdir/git_info
 
 function do_run() {
 	xrepeats=$1
-	for xarr in "da" "sla"; do
+	xarr="sla"
+	for sla_max_level in $sla_max_level_S; do
 		for rle_rec_limit in $rle_rec_limit_S; do
 			for xarr_rle_grain in $xarr_rle_grain_S; do
 				for rles in $rles_S; do
 					for threads in 1 $(seq 2 2 $(nproc)); do
 						for i in $(seq $xrepeats);  do
+							SLA_MAX_LEVEL=$sla_max_level   \
 							CILK_NWORKERS=$threads         \
 							RLE_REC_LIMIT=$rle_rec_limit   \
 							XARR_RLE_GRAIN=$xarr_rle_grain \
 							./rle/prle_rec_xarray_$xarr $rles
 						done
+					done
+				done
+			done
+		done
+	done
+
+	xarr="da"
+	for rle_rec_limit in $rle_rec_limit_S; do
+		for xarr_rle_grain in $xarr_rle_grain_S; do
+			for rles in $rles_S; do
+				for threads in 1 $(seq 2 2 $(nproc)); do
+					for i in $(seq $xrepeats);  do
+						SLA_MAX_LEVEL=$sla_max_level   \
+						CILK_NWORKERS=$threads         \
+						RLE_REC_LIMIT=$rle_rec_limit   \
+						XARR_RLE_GRAIN=$xarr_rle_grain \
+						./rle/prle_rec_xarray_$xarr $rles
 					done
 				done
 			done
